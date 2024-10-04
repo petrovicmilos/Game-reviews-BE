fc4b5ccf0df98dd66bbc4953008d8dca8ccd033a
petrovicmilos <milosp1995@gmail.com>
2024-10-04 13:00:18 +0200

    first commit

---
+++ src/main/java/com/gamereviews/gamereviews/GamereviewsApplication.java
@@ -0,0 +1,13 @@
+package com.gamereviews.gamereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamereviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamereviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamereviews/gamereviews/GamereviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamereviews.gamereviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamereviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
